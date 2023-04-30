9060919d14cad184d02119b804669f8749adaad8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-30 11:35:09 -0300

    Definindo a estilização global

---
+++ src/styles/global.jsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    body {
+        background: #f5f5f5;
+        font-size: 14px;
+        color: #333;
+        font-family: inter, sans-serif;
+    }
+
+    @media (max-width: 720px) {
+        html {
+            font-size: 87.5%;
+        }
+    }
+
+    @media (max-width: 1080px) {
+        html {
+            font-size: 93.75%;
+        }
+    }
+`;
