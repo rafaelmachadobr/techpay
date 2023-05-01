a78713706e1127843ddbdca29594e7417c2b2a33
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-05-01 15:53:14 -0300

    Componente Header

---
+++ src/components/Header/styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100%;
+  height: 60px;
+  background: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  padding: 0 30px;
+`;
